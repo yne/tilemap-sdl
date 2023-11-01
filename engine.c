ceb9cae9f49c0ab730c2f012968673b2c791f4fc
Rémy F <yne@users.noreply.github.com>
2023-11-01 19:58:44 +0100

    Handle diagonal wall + add demo root

--- engine.c
+++ engine.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
 #define MAX(A, B) (A > B ? A : B)
 #define MIN(A, B) (A < B ? A : B)
 #define u8str(u) hex(u >> 4), hex(u)
@@ -23,7 +24,7 @@ struct {
 struct {
     uint8_t* map[2];
     uint32_t cols, rows, total;
-    uint8_t walkable, colision, swimming, paddling, sloshing, solid_ne, solid_se, solid_nw, solid_sw, teleport, slippery, bridge_h, bridge_v, stair_ru, stair_rd;
+    uint8_t walkable, colision, swimming, paddling, sloshing, solid_se, solid_sw, solid_ne, solid_nw, slippery, teleport, bridge_h, bridge_v, stair_ru, stair_rd;
 } tilemap;
 struct {
     SDL_Texture* texture[2];
@@ -37,73 +38,106 @@ struct {
 } layers;
 struct {
     SDL_Texture* texture;
-    uint32_t size, dir, step, speed;
-    int32_t x, y; // player position on the worldmap
-    int32_t velocity_x, velocity_y; // when moving on ice/water
+    uint32_t dir, step, speed;
+    int32_t size, x, y, old_x, old_y; // in pixel
     _Bool debug, dead, god, on_bridge_from_side;
 } player;
-
-uint8_t old_tile_idx = 0;//TODO move into player ?
-uint8_t old_tile_x = 0;
-uint8_t old_tile_y = 0;
 
 char hex(uint8_t u)
 {
     u &= 0xf;
     return (u >= 0 && u <= 9) ? '0' + u : 'A' + u - 10;
 }
-
+uint8_t getTileAt(uint32_t x, uint32_t y) {
+    int32_t tile_x = x / tileset.size; // (player.x + (player.size / 2))
+    int32_t tile_y = y / tileset.size; // (player.y + player.size - 4)
+    return tilemap.map[0][tile_x + (tile_y * tilemap.cols)];
+}
 void movePlayer(int32_t dx, int32_t dy)
 {
+    int32_t old_x = player.x;
+    int32_t old_y = player.y;
     if (player.x + dx > tilemap.cols * tileset.size)
         dx = 0;
     if (player.y + dy > tilemap.rows * tileset.size)
         dy = 0;
-    int32_t tile_x = (dx + player.x) / tileset.size; // (player.x + (player.size / 2))
-    int32_t tile_y = (dy + player.y) / tileset.size; // (player.y + player.size - 4)
-    uint8_t tile_idx = tilemap.map[0][tile_x + (tile_y * tilemap.cols)];
-    if (player.god || tile_idx >= tilemap.walkable) {
+    uint8_t tile_idx = getTileAt(player.x + dx, player.y + dy);
+    if (player.god || tile_idx <= tilemap.walkable) {
         player.x += dx;
         player.y += dy;
-    } else if (tile_idx >= tilemap.colision) {
-    } else if (tile_idx >= tilemap.swimming) {
+    } else if (tile_idx <= tilemap.colision) {
+    } else if (tile_idx <= tilemap.swimming) {
         player.x += dx / 2;
         player.y += dy / 2;
-    } else if (tile_idx >= tilemap.solid_ne) {
-        player.x -= MAX(MAX(dy, 0), MAX(dy, 0)); // x-- if player goes up
-        player.y += dy;// y-- if player goes right
-    } else if (tile_idx >= tilemap.solid_se) { // x-- if player goes in the solid
-    } else if (tile_idx >= tilemap.solid_nw) {
-    } else if (tile_idx >= tilemap.solid_sw) {
-    } else if (tile_idx >= tilemap.teleport) {
-        printf("teleport[%i:%i]", tile_x, tile_y);
-    } else if (tile_idx < tilemap.slippery) {
-        player.x += player.velocity_x = MAX(player.velocity_x, dx);
-        player.y += player.velocity_y = MAX(player.velocity_y, dy);
-    } else if (tile_idx >= tilemap.bridge_h) {
-        if (old_tile_idx >= tilemap.bridge_h) { // still on the bridge
+    } else if (tile_idx <= tilemap.paddling) {
+        player.x += dx / 2;
+        player.y += dy / 2;
+    } else if (tile_idx <= tilemap.sloshing) {
+        player.x += dx / 2;
+        player.y += dy / 2;
+        //SE SW NE NW
+    } else if (tile_idx <= tilemap.solid_se) {
+        int32_t tile_x = (player.x + dx) % tileset.size;
+        int32_t tile_y = (player.y + dy) % tileset.size;
+        if (tile_x < (tileset.size - tile_y)) { //not in the N-E corner: can move
+            player.x += dx;
+            player.y += dy;
+        }
+    } else if (tile_idx <= tilemap.solid_sw) {
+        int32_t tile_x = (player.x + dx) % tileset.size;
+        int32_t tile_y = (player.y + dy) % tileset.size;
+        if (tile_x > tile_y) { // not in the N-W corner: can move
+            player.x += dx;
+            player.y += dy;
+        }
+    } else if (tile_idx <= tilemap.solid_ne) {
+        int32_t tile_x = (player.x + dx) % tileset.size;
+        int32_t tile_y = (player.y + dy) % tileset.size;
+        if (tile_x < tile_y) { // not in the N-W corner: can move
+            player.x += dx;
+            player.y += dy;
+        }
+    } else if (tile_idx <= tilemap.solid_nw) {
+        int32_t tile_x = (player.x + dx) % tileset.size;
+        int32_t tile_y = (player.y + dy) % tileset.size;
+        if (tile_x > (tileset.size - tile_y)) { //not in the N-E corner: can move
+            player.x += dx;
+            player.y += dy;
+        }
+    } else if (tile_idx <= tilemap.slippery) {
+        int32_t h = (player.x - player.old_x)?:dx;
+        player.x += h;
+        if(!h)player.y += (player.y - player.old_y)?:dy;//only allow 1 slide direction
+    } else if (tile_idx <= tilemap.teleport) {
+        player.x=0xD0;
+        player.y=0xA0;
+    } else if (tile_idx <= tilemap.bridge_h) {
+        uint8_t old_tile = getTileAt(player.x, player.y);
+        _Bool was_on_bridge = tilemap.teleport < old_tile && old_tile <= tilemap.bridge_h;
+        if (was_on_bridge) { // still on the bridge
             player.x += dx;
             player.y += dy;
         } else { // we entered the bridge
-            int on_bridge_from_side = old_tile_x == tile_x;
+            //int on_bridge_from_side = old_tile_x == tile_x;
         }
-    } else if (tile_idx >= tilemap.bridge_v) {
-        if (old_tile_idx >= tilemap.bridge_h) { // still on the bridge
+    } else if (tile_idx <= tilemap.bridge_v) {
+        uint8_t old_tile = getTileAt(player.x, player.y);
+        _Bool was_on_bridge = tilemap.bridge_h < old_tile && old_tile <= tilemap.bridge_v;
+        if (was_on_bridge) { // still on the bridge => can move
             player.x += dx;
             player.y += dy;
-        } else { // we entered the bridge
-            int on_bridge_from_side = old_tile_x == tile_x;
+        } else{
+            //int on_bridge_from_side = old_tile_x == tile_x;
         }
-    } else if (tile_idx < tilemap.stair_ru) {
+    } else if (tile_idx <= tilemap.stair_ru) {
         player.x += (dx + dy);
         player.y += (dx + dy);
-    } else if (tile_idx >= tilemap.stair_rd) {
+    } else if (tile_idx <= tilemap.stair_rd) {
         player.x += (-dx + -dy);
         player.y += (-dx + -dy);
     }
-    old_tile_idx = tile_idx;
-    old_tile_x = tile_x;
-    old_tile_y = tile_y;
+    player.old_x = old_x;
+    player.old_y = old_y;
 }
 // no SDL_LoadFile* on SDL<2.0.6
 void* loadFile(char* path, size_t* size)
@@ -150,6 +184,7 @@ _Bool HandleEvent()
         if (sc == SDL_SCANCODE_F)
             SDL_SetWindowFullscreen(view.window, (SDL_GetWindowFlags(view.window) & SDL_WINDOW_FULLSCREEN) ^ SDL_WINDOW_FULLSCREEN);
     }
+    movePlayer(0,0);// continue sliding
     return moved;
 }
 
@@ -187,17 +222,18 @@ void init(char* ini)
         tilemap.map[1] = loadFile(overlay, &tilemap_size);
         layers.texture[1] = SDL_CreateTexture(view.render, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, layers.width, layers.height);
         tileset.texture[1] = SDL_CreateTextureFromSurface(view.render, SDL_LoadBMP(getVal(conf, conf_len, "TILESET1=")));
-        SDL_SetTextureBlendMode(layers.texture[1], SDL_BLENDMODE_BLEND);
+        SDL_SetTextureBlendMode(tileset.texture[1], SDL_BLENDMODE_NONE); // copying tiles shall replace targeted ones
+        SDL_SetTextureBlendMode(layers.texture[1], SDL_BLENDMODE_BLEND); // copying this layer shall blend the alpha
     }
-    tilemap.walkable = atoi(getVal(conf, conf_len, "WALKABLE=")?:"0");
+    tilemap.walkable = atoi(getVal(conf, conf_len, "WALKABLE=")?:"99");
     tilemap.colision = atoi(getVal(conf, conf_len, "COLISION=")?:"0");
     tilemap.swimming = atoi(getVal(conf, conf_len, "SWIMMING=")?:"0");
     tilemap.paddling = atoi(getVal(conf, conf_len, "PADDLING=")?:"0");
     tilemap.sloshing = atoi(getVal(conf, conf_len, "SLOSHING=")?:"0");
-    tilemap.solid_ne = atoi(getVal(conf, conf_len, "SOLID_NE=")?:"0");
     tilemap.solid_se = atoi(getVal(conf, conf_len, "SOLID_SE=")?:"0");
-    tilemap.solid_nw = atoi(getVal(conf, conf_len, "SOLID_NW=")?:"0");
     tilemap.solid_sw = atoi(getVal(conf, conf_len, "SOLID_SW=")?:"0");
+    tilemap.solid_ne = atoi(getVal(conf, conf_len, "SOLID_NE=")?:"0");
+    tilemap.solid_nw = atoi(getVal(conf, conf_len, "SOLID_NW=")?:"0");
     tilemap.teleport = atoi(getVal(conf, conf_len, "TELEPORT=")?:"0");
     tilemap.slippery = atoi(getVal(conf, conf_len, "SLIPPERY=")?:"0");
     tilemap.bridge_h = atoi(getVal(conf, conf_len, "BRIDGE_H=")?:"0");
@@ -221,12 +257,9 @@ void fini()
 
 void player_draw()
 {
-    &(SDL_Rect) { player.x /*- view.x*/ + 2, player.y /*- view.y*/ + player.size - 2, player.size - 4, 2 };
-    SDL_RenderFillRect(view.render,
-        &(SDL_Rect) { player.x - view.x + 2, player.y - view.y + player.size - 2, player.size - 4, 2 });
     SDL_RenderCopy(view.render, player.texture,
         &(SDL_Rect) { 0, (player.dir * 2 + player.step) * player.size, player.size, player.size },
-        &(SDL_Rect) { player.x - view.x, player.y - view.y, player.size, player.size });
+        &(SDL_Rect) { player.x - view.x - (player.size/2), player.y - view.y - player.size, player.size, player.size });
 }
 
 void layer_render(uint8_t level)
@@ -324,9 +357,15 @@ int main(int argc, char** argv)
         player_draw();
         layer_render(1);
 
+
         printXY(6, 8, (char[]) { 'C', 'A', 'M', ':', u16str(view.x), ' ', u16str(view.y), 0 });
-        printXY(6, 24, (char[]) { 'U', 'S', 'R', ':', u16str(player.x), ' ', u16str(player.y), 0 });
-        printXY(6, 32, (char[]) { 'G', 'O', 'D', ':', player.god ? 'Y' : 'N', 0 });
+        printXY(6, 16, (char[]) { 'U', 'S', 'R', ':', u16str(player.x), ' ', u16str(player.y), 0 });
+        printXY(6, 24, (char[]) { 'G', 'O', 'D', ':', player.god ? 'Y' : 'N', 0 });
+        SDL_RenderCopy(view.render, tileset.texture[0],
+            &(SDL_Rect) { 0, getTileAt(player.x,player.y) * tileset.size, tileset.size, tileset.size },
+            &(SDL_Rect) { 6, 40, tileset.size, tileset.size });
+        SDL_SetRenderDrawColor(view.render, 0xFF, 0x00, 0x00, 0xFF);
+        SDL_RenderDrawPoint(view.render, 6 + player.x%tileset.size, 40 + player.y%tileset.size);
         SDL_RenderPresent(view.render);
     }
     fini();
